66f80cac0144fe33ca36166c005557f00b99f5ff
motwnb <98505614+MotWnb@users.noreply.github.com>
2025-09-07 14:50:41 +0800

    Initial commit

---
+++ src/client/java/com/motwnb/clienttoys/ClientToysClient.java
@@ -0,0 +1,10 @@
+package com.motwnb.clienttoys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientToysClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
